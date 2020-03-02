8248e0ef26af8aacf06a11c0e4190ae2c0ab1033
Francisco REY ZALUAR <francisco.rey-zaluar@dauphine.eu>
2020-03-02 23:10:28 +0100

    Added editBox interaction

---
+++ ExampleClassLibrary/COMFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ExampleClassLibrary
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class DataPair
+    {
+        public string Label { get; set; }
+        public int LabelID { get; set; }
+    }
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class ExampleClassLibraryCOMFunctions
+    {
+        public DataPair GetDataPair(string label, int labelid)
+        {
+            return new DataPair() { Label = label, LabelID = labelid };
+        }
+
+        public DateTime ThirtyDaysAgo()
+        {
+            return DateTime.Today - TimeSpan.FromDays(30);
+        }
+    }
+}
--- ExampleClassLibrary/ExampleClass.cs
+++ ExampleClassLibrary/ExampleClass.cs
@@ -4,35 +4,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 using ExcelDna;
-using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
 using Microsoft.Office.Core;
 
 
 namespace ExampleClassLibrary
 {
-    public class ExampleClass
+    [ComVisible(true)]
+    public class ExampleClass : ExcelDna.Integration.CustomUI.ExcelRibbon
     {
+        private string _myValue;
         public void OnExample1ButtonPressed(IRibbonControl control)
         {
             MessageBoxResult mbr = MessageBox.Show("Enter Yes or No into selected Cell?",
                                                     "Choose",
                                                     MessageBoxButton.YesNo);
-            Microsoft.Office.Interop.Excel.Application excel_application =
-                (Microsoft.Office.Interop.Excel.Application)ExcelDna.Integration.ExcelDnaUtil.Application;
+            Excel.Application excel_application =
+                (Excel.Application)ExcelDna.Integration.ExcelDnaUtil.Application;
 
             string result = (mbr == MessageBoxResult.Yes) ? "Yes" : "No";
-
+            
             object selection = excel_application.Selection;
-            if (selection is Microsoft.Office.Interop.Excel.Range)
+            if (selection is Excel.Range)
             {
-                Microsoft.Office.Interop.Excel.Range selected_range =
-                    (Microsoft.Office.Interop.Excel.Range)selection;
+                Excel.Range selected_range =
+                    (Excel.Range)selection;
                 int first_col = selected_range.Column;
                 int first_row = selected_range.Row;
 
-                selected_range.Worksheet.Cells[first_row, first_col].Value = result;
+                selected_range.Worksheet.Cells[first_row, first_col].Value = _myValue;
+            }
+        }
+
+        public void GetEditBoxValue(IRibbonControl control, string text=null)
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                _myValue = text;
             }
         }
     }
